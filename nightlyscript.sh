120a0c5223e6097a44d0878a013d2f89d9bb4ced
ezjanos <oskar.jansson@ericsson.com>
2021-12-03 16:16:34 +0100

    fixes: nightlyscripts variables

--- nightlyscript.sh
+++ nightlyscript.sh
@@ -8,4 +8,4 @@ REPOPATH=
 $PYPATH $AOCURLPATH $YEARVAR $DAYVAR;
 
 mkdir $REPOPATH/day$DAYVAR && cp $REPOPATH/template/* $REPOPATH/day$DAYVAR;
-echo -e "CC=g++\nCCstd=c++11\n.SILENT:\nall:\n\t\$(CC) -std=\$(CCstd) main.cc ../shared/execution.cc\n\t/app/vbuild/RHEL7-x86_64/python/3.8.0/bin/python3 /home/ezjanos/Scripts/aocurl.py \$(YEARVAR) \$(DAYVAR) -io | ./a.out" > $REPOPATH/day$DAYVAR/Makefile
+echo -e "CC=g++\nCCstd=c++11\n.SILENT:\nall:\n\t\$(CC) -std=\$(CCstd) main.cc ../shared/execution.cc\n\t/app/vbuild/RHEL7-x86_64/python/3.8.0/bin/python3 /home/ezjanos/Scripts/aocurl.py $(YEARVAR) $(DAYVAR) -io | ./a.out" > $REPOPATH/day$DAYVAR/Makefile
